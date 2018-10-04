0b0f83c8f09c6ad6b738a3e9ddc0a3d7209d0b78
pemathinley <42267591+pemathinley@users.noreply.github.com>
2018-10-04 19:27:36 +0530

    Add files via upload

--- intersection.java
+++ intersection.java
@@ -1,111 +1,60 @@
-import java.util.;
+import java.util.*;
+public class intersection
+{
+	private static Scanner ip = new Scanner(System.in);
 
-class MyArray{
-    
-    int arr[];
-
-    int index;
-
-    int size;
-    
-    MyArray(int size){
-        
-        index=-1;
-
-        this.size = size;
-
-        arr=new int[size];
-    }
-    
-    void add(int data) {
-        
-        if(index==size-1){
-            
-            int temp[]=arr;
-            
-            size=size2;
-            
-            arr=new int[size];
-            
-            System.arraycopy(temp, 0, arr, 0, temp.length);
-            
-            arr[++index]=data;
-        }
-        
-        else
-            arr[++index]=data;
-        
-    }
-    
-   String findIntersection(MyArray a){
-        
-        String s=;
-       
-        int freq1[]=new int[10];
-        
-        int freq2[]=new int[10];
-        
-        for(int x arr){
-            freq1[x]++;
-        }
-        for(int x a.arr){
-            freq2[x]++;
-        }
-        
-        for(int i=0; i10; i++){
-            
-            if(freq1[i]0 && freq2[i]0){
-                
-                int min=freq1[i]  freq2[i]  freq1[i]  freq2[i];
-                
-                min=2;
-                
-                while(min0){
-                    
-                    s+=i+ ;
-                    min--;
-                }
-            }
-        }
-        return s;
-    }
-}    
-public class intersection {
-    
-    public static void main(String args[]){
-        
-        Scanner sc=new Scanner(System.in);
-        
-        int n=sc.nextInt();
-        
-        MyArray a1=new MyArray(n);
-        
-        for(int i=0; in; i++){
-            
-            int x = sc.nextInt();
-            
-            a1.add(x);
-        }
-       
-        a1.display();
-        
-        MyArray a2=new MyArray(n);
-        
-        for(int i=0; in; i++){
-            
-            int x = sc.nextInt();
-            
-            a2.add(x);
-        }
-        
-        a2.display();
-        
-        String s= a1.findIntersection(a2);
-        
-       
-            System.out.print(s);
-        
-    sc.close();
-    
-    }
+	public List<Integer> intersection(int arr1[], int arr2[]) 
+	{
+		List<Integer> intersect1 = new ArrayList<Integer>();
+		List<Integer> intersect2 = new ArrayList<Integer>();
+		List<Integer> intersect3 = new ArrayList<Integer>();
+	
+		for (int i: arr1) {
+			intersect1.add(i);
+		}
+		
+		for (int i: arr2) {
+			intersect2.add(i);
+		}
+		
+		for (int i: arr2) {
+			int occ1 = Collections.frequency(intersect1,i);
+			int occ2 = Collections.frequency(intersect2,i);
+			if (occ1 != 0 && !(intersect3.contains(i))) { 
+				if (occ1 < occ2)
+					while (occ1-- != 0)
+						intersect3.add(i);
+				else if (occ2 < occ1)
+					while (occ2-- != 0)
+						intersect3.add(i);
+				else if (occ2 == occ1)
+					while (occ1-- != 0)
+						intersect3.add(i);
+			}
+		}
+		Collections.sort(intersect3);
+		return intersect3;
+	}
+	
+	public static void main(String []args) 
+	{
+		IntersectionOfElement obj = new IntersectionOfElement();
+		System.out.println("Enter First Array size");
+		int n1 = ip.nextInt();
+		
+		int arr1[] = new int[n1];
+		System.out.println("Enter first elements : ");
+		for (int i=0;i<n1;i++)
+			arr1[i] = ip.nextInt();
+		
+		System.out.println("Enter Second Array size");
+		int n2 = ip.nextInt();
+		
+		int arr2[] = new int[n2];
+		System.out.println("Enter second elements : ");
+		for (int i=0;i<n2;i++)
+			arr2[i] = ip.nextInt();
+		
+		System.out.println(obj.intersection(arr1,arr2).toString());
+	}
 }
